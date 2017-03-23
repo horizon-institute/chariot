19965b144f84f1730358983367a3b247d712fd81
Kevin Glover <ktg@cs.nott.ac.uk>
2017-03-23 14:07:36 +0000

    Fix button enabling/disabling

--- src/www/scripts/graph.js
+++ src/www/scripts/graph.js
@@ -16,15 +16,15 @@ function qs(key) {
 $(document).ready(function () {
 	var chartDiv = $('#chart');
 	chartDiv.logger({
-		'all_channels': qs('channels') == 'all'
+		'all_channels': qs('channels') === 'all'
 	});
 	chartDiv.bind("logger:click", function (event, params) {
-		if (params.button == 'clear_selection') {
-			$("#zoomIn").prop("disabled", true);
+		if (params.button === 'clear_selection') {
+			$("#zoomIn").MaterialButton.disable();
 			$("#addAnnotation").hide();
 		}
-		else if (params.button == 'create_selection') {
-			$("#zoomIn").prop("disabled", false);
+		else if (params.button === 'create_selection') {
+			$("#zoomIn").MaterialButton.enable();
 			var annotationItem = $('#toggleAnnotations');
 			var showAnnotations = annotationItem.data('show');
 			if (showAnnotations) {
@@ -71,11 +71,11 @@ $(document).ready(function () {
 					startDate: dataStartDay,
 					endDate: dataEndDay
 				}];
-				$("#zoomOut").prop("disabled", false);
+				$("#zoomOut").MaterialButton.enable();
 			}
 			else {
 				zooms = [];
-				$("#zoomOut").prop("disabled", true);
+				$("#zoomOut").MaterialButton.disable();
 			}
 			update_date_range(startDay, endDay);
 		});
@@ -135,18 +135,22 @@ $(document).ready(function () {
 				startDate: startDate,
 				endDate: endDate
 			});
-			$("#zoomOut").prop("disabled", false);
+			$("#zoomOut").MaterialButton.enable();
 			var start = moment(fe.logger.plot.get_time_for_x(selection.x));
 			var end = moment(fe.logger.plot.get_time_for_x(selection.x + selection.w));
 			fe.logger.plot.clear_selection();
 			update_date_range(start, end);
 		}
-	}).prop("disabled", true);
+	});
 	$("#zoomOut").click(function () {
 		var zoom = zooms.pop();
-		$("#zoomOut").prop("disabled", zooms.length == 0);
+		if(zooms.length === 0) {
+			$("#zoomOut").MaterialButton.disable();
+		} else {
+			$("#zoomOut").MaterialButton.enable();
+		}
 		update_date_range(zoom.startDate, zoom.endDate);
-	}).prop("disabled", true);
+	});
 
 	update_date_range(moment(dataStart).startOf('day'), moment(dataEnd).endOf('day'));
 	$(window).resize(function () {
@@ -203,9 +207,9 @@ function build_menu(sensors) {
 	});
 
 	$.each(channels, function (channelID, channel) {
-		if (selected_axis_l == undefined) {
+		if (selected_axis_l === undefined) {
 			selected_axis_l = channelID;
-		} else if (selected_axis_r == undefined) {
+		} else if (selected_axis_r === undefined) {
 			selected_axis_r = channelID;
 		}
 
@@ -237,7 +241,7 @@ function build_menu(sensors) {
 					var selected = true;
 					var filterIcon = 'visibility';
 
-					if (filtered_channels.indexOf(key) != -1) {
+					if (filtered_channels.indexOf(key) !== -1) {
 						selected = false;
 						filterIcon = 'visibility_off';
 					}
@@ -248,14 +252,14 @@ function build_menu(sensors) {
 					var avg_duration = 0;
 					var previous = null;
 					$.each(stream.data, function (i, dataItem) {
-						if (value_min == null || dataItem.value < value_min) {
+						if (value_min === null || dataItem.value < value_min) {
 							value_min = dataItem.value;
 						}
-						if (value_max == null || dataItem.value > value_max) {
+						if (value_max === null || dataItem.value > value_max) {
 							value_max = dataItem.value;
 						}
 
-						if (previous != null) {
+						if (previous !== null) {
 							var prev_duration = dataItem.time - previous.time;
 							var total_duration = prev_duration + avg_duration;
 
@@ -268,7 +272,7 @@ function build_menu(sensors) {
 
 					var content = $('<div></div>')
 						.append('<div>' + (device.location || device.name || 'Sensor') + '</div>');
-					if (value_min != null && value_max != null) {
+					if (value_min !== null && value_max !== null) {
 						content.append('<div class="mdl-typography--caption-color-contrast">' +
 							Number(value_min.toFixed(1)) + stream.units + ' – ' +
 							Number(value_max.toFixed(1)) + stream.units +
@@ -342,7 +346,7 @@ function build_menu(sensors) {
 		visibilityMenu.append(channelSection);
 	});
 
-	if (selected_axis_r == undefined) {
+	if (selected_axis_r === undefined) {
 		selected_axis_r = selected_axis_l;
 	}
 
@@ -361,17 +365,17 @@ function select_axes(new_channel) {
 
 	var unique_channels = [];
 	$.each(streams, function (index, stream) {
-		if (unique_channels.indexOf(stream.channel) == -1) {
+		if (unique_channels.indexOf(stream.channel) === -1) {
 			unique_channels.push(stream.channel);
 		}
 	});
 
 	var curr = [selected_axis_l, selected_axis_r];
 
-	if (unique_channels.length == 1) {
+	if (unique_channels.length === 1) {
 		// If only 1 channel is selected, and it's not the left-most,
 		// make it the left axis.
-		if (selected_axis_l != unique_channels[0]) {
+		if (selected_axis_l !== unique_channels[0]) {
 			select_axis_channel(unique_channels[0], true);
 			select_axis_channel(unique_channels[0], false);
 		}
@@ -379,7 +383,7 @@ function select_axes(new_channel) {
 	else if (unique_channels.length > 1) {
 		// If >1 channel is selected, and we've selected a 
 		// new channel, make it the right-most.
-		if (new_channel && curr.indexOf(new_channel) == -1) {
+		if (new_channel && curr.indexOf(new_channel) === -1) {
 			select_axis_channel(new_channel, false);
 		}
 		else {
@@ -387,12 +391,12 @@ function select_axes(new_channel) {
 			// an axis, then swap for a different one.
 			var lhs_pos = unique_channels.indexOf(selected_axis_l);
 			var rhs_pos = unique_channels.indexOf(selected_axis_r);
-			if (lhs_pos == -1) {
+			if (lhs_pos === -1) {
 				// Swap the left axis for one that exists.
 				unique_channels.splice(rhs_pos, 1);
 				select_axis_channel(unique_channels[unique_channels.length - 1], true);
 			}
-			else if (rhs_pos == -1) {
+			else if (rhs_pos === -1) {
 				// Swap the right axis for one that exists.
 				unique_channels.splice(lhs_pos, 1);
 				select_axis_channel(unique_channels[unique_channels.length - 1], false);
@@ -405,7 +409,7 @@ var select_axis_channel = function (key, left) {
 	var radio_name = left ? "input[name=axis-left]" : "input[name=axis-right]";
 	$(".axis-item").each(function () {
 		var channel = $(this).data('channel-id');
-		if (channel == key) {
+		if (channel === key) {
 			$(this).find(radio_name).attr("checked", true);
 		}
 		else {

eefa59aad32cd79db677e44206d9514f43785234
Kevin Glover <ktg@cs.nott.ac.uk>
2017-03-23 13:10:40 +0000

    Update migrations

--- src/app/hubs/models.py
+++ src/app/hubs/models.py
@@ -10,8 +10,6 @@ from django.utils.translation import ugettext_lazy as _
 class Hub(models.Model):
     id = models.CharField(_('MAC Address'), primary_key=True, max_length=255)
     name = models.CharField(_('Name'), max_length=255)
-    external_network_address = models.GenericIPAddressField(blank=True, null=True)
-    network_address = models.GenericIPAddressField(blank=True, null=True)
     online_since = models.DateTimeField(blank=True, null=True)
     last_ping = models.DateTimeField(blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
